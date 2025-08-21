941f6564feb0935aeb7512b27ebed7f5b5c50287
ZawHeinHtet88 <rainbows3338@gmail.com>
2025-08-21 23:05:54 +0630

    fix :something

--- src/modules/orders/hooks/queries.tsx
+++ src/modules/orders/hooks/queries.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query"
 import { getOrders } from "../api"
 import type { BasePagination } from "@/types/global"
 
-
 export const useGetAllOrdersQuery = (filters:BasePagination) => {
     return useQuery({
         queryKey : ["orders",filters],
